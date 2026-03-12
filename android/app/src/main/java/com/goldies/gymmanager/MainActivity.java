37d0788183b7ee33f3cecdc9edb0643684dcb47e
madhavraj2004 <raj.madhav2004@gmail.com>
2026-03-12 23:09:57 +0530

    update android build and fixes

---
+++ android/app/src/main/java/com/goldies/gymmanager/MainActivity.java
@@ -0,0 +1,5 @@
+package com.goldies.gymmanager;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,13 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.0a59e243a4a347b68d37fce9ec361bf7',
-  appName: "Goldie's Gym",
-  webDir: 'dist',
-  server: {
-    url: 'https://0a59e243-a4a3-47b6-8d37-fce9ec361bf7.lovableproject.com?forceHideBadge=true',
-    cleartext: true,
-  },
+  appId: 'com.goldies.gymmanager',
+  appName: 'Goldies Gym Manager',
+  webDir: 'dist'
 };
 
 export default config;
